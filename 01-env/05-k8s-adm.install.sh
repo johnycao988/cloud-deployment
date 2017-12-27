2078792f02c255b43dbc94dfa0b6ab0ed1d9e529
johnnycao“TestCIgit config --global user.na.r.johnnycao“ <johnnycao@chinasystems.com>
2017-12-27 15:46:04 +0800

    11

---
+++ 01-env/05-k8s-adm.install.sh
@@ -0,0 +1,22 @@
+## Master server ,first need to run "chmod +x *.sh" in command line 
+
+#!/bin/sh
+# config yum repo.
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+setenforce 0
+yum install -y kubelet kubeadm kubectl
+systemctl enable kubelet && systemctl start kubelet
+
+ 
+
+
