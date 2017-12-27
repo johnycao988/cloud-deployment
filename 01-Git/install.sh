cb1ca6efd4cd91162eb6329403441f65e3f70e79
johnnycao“TestCIgit config --global user.na.r.johnnycao“ <johnnycao@chinasystems.com>
2017-12-27 09:45:21 +0800

    11

--- 01-Git/install.sh
+++ 01-Git/install.sh
@@ -1,9 +1,9 @@
-## Master server 
+## Master server ,first need to run "chmod +x install.sh" in command line 
 
 #!/bin/sh
 
 ### step1: install git 
-yum install git
+#yum install git
 
 ### step2: config
 git config --global user.name "Johnny Cao"
