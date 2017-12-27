c0ec826f0d2425630266852434e7101b57b89d9b
johnnycao“TestCIgit config --global user.na.r.johnnycao“ <johnnycao@chinasystems.com>
2017-12-27 09:52:57 +0800

    11

--- 01-Git/install.sh
+++ 01-Git/install.sh
@@ -3,16 +3,17 @@
 #!/bin/sh
 
 ### step1: install git 
+echo "Installing git..."
 yum install git
 
 ### step2: config
+echo "Configing git..."
 git config --global user.name "Johnny Cao"
 git config --global user.email  "johnnycao@chinasystems.com"
 ## No password for git commit
 git config --global credential.helper store
 
-## Not commit after change file access.
-git config core.fileMode false
+## Not commit after change file access. 
 
 echo "Successfully install and config git."
 
