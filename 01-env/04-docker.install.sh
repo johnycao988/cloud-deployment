2880f3618f112701231447bf566d96c9552289e6
johnnycao“TestCIgit config --global user.na.r.johnnycao“ <johnnycao@chinasystems.com>
2017-12-27 15:32:38 +0800

    11

--- 01-env/04-docker.install.sh
+++ 01-env/04-docker.install.sh
@@ -8,9 +8,7 @@ export _CSAST='ansible -i ./ansible-hosts'
 $_CSAST all -m command -a 'yum install -y docker'
 
 ## set docker registry
-$_CSAST all -m command -a 'cat << EOF > /etc/docker/daemon.json
-{"insecure-registries":["10.39.101.226:5000"] } 
-EOF'
+$_CSAST -m copy -a 'src=./docker-daemon.json dest=/etc/docker/daemon.json'
  
 ## enable and start docker service
 $_CSAST all -m command -a 'systemctl enable docker && systemctl start docker'
