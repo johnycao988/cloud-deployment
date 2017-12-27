1c79eb8d10cf1f3ec12312cf828699551c63c37c
johnnycao“TestCIgit config --global user.na.r.johnnycao“ <johnnycao@chinasystems.com>
2017-12-27 10:01:45 +0800

    11

--- 02-Ansible/install.sh
+++ 02-Ansible/install.sh
@@ -1,6 +1,10 @@
-## Master server 
-#!/bin/sh
-### step1: install ansible 
-yum install ansible
+## Master server ,first need to run "chmod +x install.sh" in command line 
 
+#!/bin/sh
+
+### step1: install ansible
+echo "Installing ansible..."
+yum install ansible
+ 
+echo "Successfully install ansible"
 
