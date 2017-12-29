611dcaed067176d7c6c88b7977da57095cc9b659
johnnycao“TestCIgit config --global user.na.r.johnnycao“ <johnnycao@chinasystems.com>
2017-12-29 12:59:47 +0800

    11

--- 01-env-setup/07-k8s.nodes.join.sh
+++ 01-env-setup/07-k8s.nodes.join.sh
@@ -3,10 +3,13 @@
 #!/bin/sh
 
 export _CSAST='ansible -i ./ansible-hosts k8s-nodes -m'
+
 ### before run below cmd, need to set value of '--token' and --discovery-token-ca-cert-hash
 $_CSAST command -a 'kubeadm join --token d239e7.9b11c0ac56999c2c 10.39.101.226:6443 --discovery-token-ca-cert-hash sha256:4ef00683d596d022f449e6e10e9ba27557074c4cb3ab0bdf8eb92cd8b9cfbf30' 
  
-# you can reset kubernetes
-# on master: kubeadm delete node nodeName 
-# on node: kubeadm reset && setenforce 0 
+###  reset kubernetes
+# on master:
+# kubeadm delete node nodeName 
+# on node: 
+# kubeadm reset && setenforce 0 
 
