f02a50243a92a1d7baa0ecdd403ebcab0f6fecb5
johnnycao“TestCIgit config --global user.na.r.johnnycao“ <johnnycao@chinasystems.com>
2017-12-29 08:59:59 +0800

    11

--- 01-env-setup/03-ssh.config.sh
+++ 01-env-setup/03-ssh.config.sh
@@ -2,11 +2,12 @@
 
 #!/bin/sh
 ## genereate key on master server, login with user: root
+
 ssh-keygen 
 
 ## copy public key to each nodes
-ssh-copy-id root@ root@10.39.107.140 
-ssh-copy-id root@ root@10.39.107.124 
-ssh-copy-id root@ root@10.39.101.226 
+ssh-copy-id root@10.39.107.140 
+ssh-copy-id root@10.39.107.124 
+ssh-copy-id root@10.39.101.226 
 # ...
  
